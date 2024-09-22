36abb649a59cae7b8a4e72134b4ac317aa3487a9
Hamza <hamzafougahli@gmail.com>
2024-09-22 10:24:49 +0100

    Added servicesReducer in Redux Store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import formReducer from "../features/formSlice";
 import contactFormReducer from "../features/contactFormSlice";
 import userReducer from "../features/userSlice";
+import servicesReducer from "../features/servicesSlice";
 
 export const store = configureStore({
   reducer: {
     form: formReducer,
     contactForm: contactFormReducer,
     users: userReducer,
+    services: servicesReducer,
   },
 });
 
